a375744c4a78a88ed512e020bfa0ec0ae3af8447
meera2003 <118628000+meera2003@users.noreply.github.com>
2024-05-14 18:48:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+  // Smooth scrolling effect to navigation links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  // Image carousel
+  let slideIndex = 0;
+
+  function showSlides() {
+    const slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+
+  showSlides();
